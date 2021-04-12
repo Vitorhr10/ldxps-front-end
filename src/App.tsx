a1ea8dd075ed053ed2cef9a89d59941c59d1c326
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-04-12 02:27:38 -0300

    fix: Removed React import because it is deprecated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import Routes from './routes';
+
+import './assets/styles/global.css';
 
 function App() {
   return (
-    <h1>Hello Landix</h1>
+    <Routes />
   );
 }
 
